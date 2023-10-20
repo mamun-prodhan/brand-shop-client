304b53b482369101c440cb5090476688ea881560
mamun-prodhan <mamunpr9@gmail.com>
2023-10-21 01:48:36 +0600

    banner updated

--- src/Pages/BrandItems/BrandItems.jsx
+++ src/Pages/BrandItems/BrandItems.jsx
@@ -1,15 +1,39 @@
 import { useLoaderData, useParams } from "react-router-dom";
 import BrandItem from "./BrandItem";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 const BrandItems = () => {
   const { name } = useParams();
+  const [loader, setLoader] = useState(true);
   const brandItems = useLoaderData();
-  console.log(brandItems);
+  const [imageSlider, setImageSlider] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/imageslider/${name}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setImageSlider(data);
+        setLoader(false);
+      });
+  }, []);
+
   return (
     <div className="max-w-7xl mx-auto">
       <h2>
         Brand Items {name}: {brandItems.length}
       </h2>
+      <div>
+        <h2 className="text-4xl font-bold text-center">Image slider</h2>
+        {loader && (
+          <p className="text-2xl font-bold text-red-500 text-center my-5">
+            Loading data
+          </p>
+        )}
+        {/* {!loader && <></>} */}
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-20">
         {brandItems.map((brandItem) => (
           <BrandItem key={brandItem._id} brandItem={brandItem}></BrandItem>
--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -9,16 +9,27 @@ const Banner = () => {
         alt="Banner"
       />
       <div className="absolute inset-0 bg-black opacity-50"></div>
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <h2 className=" text-white text-xl md:text-3xl lg:text-4xl text-center">
-          The Biggest
-        </h2>
-        <h2 className="my-2 md:my-4 text-white text-2xl md:text-5xl lg:text-6xl font-bold text-center">
-          Event Planner
-        </h2>
-        <p className="text-white text-sm md:text-xl  text-center tracking-[4px]">
-          Every Event Should be perfect
+      <div className="absolute top-1/2 left-1/2 md:left-[30%] transform -translate-x-1/2 -translate-y-1/2">
+        <p className="text-white text-sm md:text-xl  text-center md:text-start tracking-[4px]">
+          Quality is our Priority
         </p>
+        <h2 className="my-2 md:my-4 text-white text-2xl md:text-5xl lg:text-6xl font-bold text-center md:text-start">
+          Explore <span>Quality</span>
+        </h2>
+        <h2 className="my-2 md:my-4 text-white text-2xl md:text-5xl lg:text-6xl font-bold text-center md:text-start">
+          And <span>Performance</span>
+        </h2>
+        <p className="text-white text-sm md:text-base  text-center md:text-start hidden md:block">
+          Cutting-edge technology, high-performance devices, sleek <br />{" "}
+          design, long-lasting battery life, advanced features, reliable
+          electronics,
+          <br /> seamless user experience, innovative engineering,advanced,
+          powerful processors.
+        </p>
+        <button className="px-3 py-2 bg-[#ef4444] text-white  rounded-lg mt-6">
+          Explore Now
+        </button>
+
         {/* <div className="text-center mt-10 font-bold">
             <button className="px-6 py-2 me-4 rounded-3xl text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
               About Us
