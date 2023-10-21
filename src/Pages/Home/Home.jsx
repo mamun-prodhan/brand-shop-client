588f442fc2766921e24ce472d0685f3a4fedb74d
mamun-prodhan <mamunpr9@gmail.com>
2023-10-21 12:44:35 +0600

    navabar logo and text modified

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -8,8 +8,17 @@ const Home = () => {
   return (
     <div>
       <Banner></Banner>
-      <div className="max-w-7xl mx-auto">
-        <h2>All Brands : {brands.length}</h2>
+      <div className="max-w-7xl mx-auto px-4 md:px-0">
+        <div>
+          <h2 className="text-xl md:text-4xl font-bold text-center">
+            Our Popular Brands
+          </h2>
+          <p className="text-sm md:text-base my-1 md:my-3 text-center md:tracking-[4px]  md:font-semibold ">
+            Explore the top technology brands and their performance in our
+            platform
+          </p>
+          <hr className="h-0.5 md:h-1 w-20 md:w-40 mt-2 md:mt-0 bg-[#000] mx-auto" />
+        </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
           {brands.map((brand) => (
             <Brands key={brand._id} brand={brand}></Brands>
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -3,12 +3,13 @@ import logo from "../../../assets/logo.png";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { useContext, useState } from "react";
 import { AuthContext } from "../../../providers/AuthProvider";
+import "./Navbar.css";
 const Navbar = () => {
   const { logOut, user } = useContext(AuthContext);
   const [open, setOpen] = useState(false);
   const navlinks = (
     <>
-      <li>
+      <li className="">
         <NavLink to="/">Home</NavLink>
       </li>
       <li>
@@ -40,14 +41,15 @@ const Navbar = () => {
       });
   };
   return (
-    <div className="shadow-md w-full  top-0 left-0 bg-indigo-400">
-      <div className="ms-4 md:ms-0 md:flex justify-between items-center py-4 md:px-20">
-        <div className="relative">
-          <img src={logo} alt="" />
+    <div className="shadow-md w-full  top-0 left-0 bg-[#F2F2F2]">
+      <div className="ms-4 md:ms-0 lg:flex justify-between items-center py-4 lg:px-20">
+        <div className="relative flex items-center">
+          <img className="w-16 md:w-24" src={logo} alt="" />
+          <p className="text-xl font-bold text-[#F15E2D]">e-Shop</p>
         </div>
         <div
           onClick={() => setOpen(!open)}
-          className="absolute top-4 right-5 text-2xl block md:hidden "
+          className="absolute top-6 md:top-8 lg:top-4 right-5 text-2xl block lg:hidden z-20 "
         >
           {open ? (
             <AiOutlineClose></AiOutlineClose>
@@ -57,21 +59,44 @@ const Navbar = () => {
         </div>
         <div
           className={`${
-            open ? "top-14" : "top-[-500px]"
-          } absolute md:static w-full md:w-auto left-0 ps-4 md:ps-0 pb-4 md:pb-0  md:flex items-center md:space-x-10 bg-indigo-400 duration-500 transition-all ease-in `}
+            open ? "top-4 md:top-2" : "top-[-500px]"
+          } absolute lg:static  w-full lg:w-auto left-0 ps-4 lg:ps-0 pb-4 lg:pb-0  lg:flex items-center lg:space-x-10 bg-[#F2F2F2] duration-500 transition-all ease-in `}
         >
-          <ul className="md:flex items-center mt-4 md:mt-0 space-y-1 md:space-y-0 md:space-x-10 text-lg font-semibold md:z-auto z-[-1]">
-            {navlinks}
+          <ul className="lg:flex items-center mt-4 lg:mt-0 space-y-1 lg:space-y-0 lg:space-x-10 font-semibold lg:z-auto z-[-1]">
+            <li className="px-4 py-2 hover:text-white hover:bg-[#FF6251] rounded-md transition-all duration-300">
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li className="px-4 py-2 hover:text-white hover:bg-[#FF6251] rounded-md transition-all duration-300">
+              <NavLink to="/addProduct">Add Product</NavLink>
+            </li>
+            <li className="px-4 py-2 hover:text-white hover:bg-[#FF6251] rounded-md transition-all duration-300">
+              <NavLink to="myCart">My Cart</NavLink>
+            </li>
+            {!user && (
+              <>
+                <li className="px-4 py-2 hover:text-white hover:bg-[#FF6251] rounded-md transition-all duration-300">
+                  <NavLink to="/login">Login</NavLink>
+                </li>
+                <li className="px-4 py-2 hover:text-white hover:bg-[#FF6251] rounded-md transition-all duration-300">
+                  <NavLink to="/register">Register</NavLink>
+                </li>
+              </>
+            )}
           </ul>
           {user && (
-            <div className=" flex items-center gap-4">
-              <img
-                className="h-8 w-8 rounded-full border-2"
-                src={user.photoURL}
-                alt=""
-              />
-              <p>{user.displayName}</p>
-              <button onClick={handleSignOut} className="btn">
+            <div className=" ps-4 lg:ps-0 lg:flex items-center gap-4">
+              <div className="flex items-center gap-2">
+                <img
+                  className="h-8 w-8 rounded-full border-2"
+                  src={user.photoURL}
+                  alt=""
+                />
+                <p>{user.displayName}</p>
+              </div>
+              <button
+                onClick={handleSignOut}
+                className="px-4 py-2 my-3 lg:my-0 font-bold bg-[#8f5cff] text-white hover:text-white hover:bg-[#FF6251] rounded-md transition-all duration-300"
+              >
                 Logout
               </button>
             </div>
